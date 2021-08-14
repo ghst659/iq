633b011578f67fe1c5a05439136b7b7bc817e4c7
ghst659 <gst659@gmail.com>
2021-08-13 20:35:17 -0700

    Adding template.

---
+++ template.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+class Solution:
+    pass
+
+
+import unittest
+
+class TestSolution(unittest.TestCase):
+    def setUp(self):
+        self._s = Solution()
+
+    def tearDown(self):
+        del self._s
+
+
+if __name__ == "__main__":
+    unittest.main()
+
+# Local Variables:
+# mode: python
+# End:
