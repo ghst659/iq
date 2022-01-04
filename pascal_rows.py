5fe401f16fc2914697393ac5452b32e6ca1bccd4
ghst659 <gst659@gmail.com>
2022-01-04 00:11:36 -0800

    Conversion to generator function.

--- pascal_rows.py
+++ pascal_rows.py
@@ -1,35 +1,40 @@
 #!/usr/bin/env python3
 
+import typing
 import unittest
 
-def pascal_rows(n: int) -> list[list[int]]:
-    """Returns the first N rows of Pascal's triangle."""
-    rows = [[1]]
-    if n > 1:
-        pascal_helper(n - 1, rows)
-    return rows
-
-def pascal_helper(k: int, rows: list[list[int]]):
-    """Add a new row based on the last row."""
-    if k < 1:
-        return
-    last_row = rows[-1]
-    next_len = len(last_row) + 1
-    next_row = [0] * next_len
-    for i in range(next_len):
-        left = 0 if i == 0 else last_row[i - 1]
-        right = 0 if i == next_len - 1 else last_row[i]
-        next_row[i] = left + right
-    rows.append(next_row)
-    pascal_helper(k - 1, rows)
+def pascal_rows(n: int) -> list[tuple[int]]:
+    result = []
+    for row in generate_rows(n):
+        result.append(row)
+    return result
     
+def generate_rows(n: int) -> typing.Generator[tuple[int], None, None]:
+    """Yields successive rows of Pascal's triangle."""
+    row = [1]
+    for i in range(n):
+        last_row = row
+        last_len= len(last_row)
+        yield tuple(row)
+        row = [0] * (last_len + 1)
+        for i in range(last_len + 1):
+            left = 0 if i == 0 else last_row[i - 1]
+            right = 0 if i == last_len else last_row[i]
+            row[i] = left + right
+
 class TestSolution(unittest.TestCase):
     def test_1(self):
-        self.assertEqual(pascal_rows(1), [[1]])
+        self.assertEqual(pascal_rows(1),
+                         [(1,)])
     def test_2(self):
-        self.assertEqual(pascal_rows(2), [[1], [1, 1]])
+        self.assertEqual(pascal_rows(2),
+                         [(1,), (1, 1)])
     def test_3(self):
-        self.assertEqual(pascal_rows(3), [[1], [1, 1], [1, 2, 1]])
+        self.assertEqual(pascal_rows(3),
+                         [(1,), (1, 1), (1, 2, 1)])
+    def test_4(self):
+        self.assertEqual(pascal_rows(4),
+                         [(1,), (1, 1), (1, 2, 1), (1, 3, 3, 1)])
 
 if __name__ == "__main__":
     unittest.main()
